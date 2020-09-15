bd8dede8809d2597b8c70e787010a51c2c79f07a
jecanizarez <je.canizarez@uniandes.edu.co>
2020-09-15 15:36:52 -0500

    nombre de variables más claras, ahora si final final2

--- Ejercicio.js
+++ Ejercicio.js
@@ -12,14 +12,14 @@ http
 
                     const resp = await axios.get("https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json");
                     filas = "";
-                    datap = resp.data; 
-                    for(let i = 0; i < datap.length; i++){
-                        filas += "<tr><td> "+ datap[i].idproveedor+ "</td><td>"+ datap[i].nombrecompania+"</td><td>"+ datap[i].nombrecontacto+"</td></tr>\n";
+                    dataproveedor = resp.data; 
+                    for(let i = 0; i < dataproveedor.length; i++){
+                        filas += "<tr><td> "+ dataproveedor[i].idproveedor+ "</td><td>"+ dataproveedor[i].nombrecompania+"</td><td>"+ dataproveedor[i].nombrecontacto+"</td></tr>\n";
 
                     }
-                    data2 = data.replace("{{data23}}",filas);
+                    dataretornar = data.replace("{{data}}",filas);
                     res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(data2);
+                    res.write(dataretornar);
                     return res.end();
                 });
                 break;
@@ -27,14 +27,14 @@ http
                 fs.readFile("Clientes.html","utf-8",async function(err,data){
                     const resp = await axios.get("https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json");
                     filas = "";
-                    datap = resp.data; 
-                    for(let i = 0; i < datap.length; i++){
-                        filas += "<tr><td> "+ datap[i].idCliente+ "</td><td>"+ datap[i].NombreCompania+"</td><td>"+ datap[i].NombreContacto+"</td></tr>\n";
+                    datacliente= resp.data; 
+                    for(let i = 0; i < datacliente.length; i++){
+                        filas += "<tr><td> "+ datacliente[i].idCliente+ "</td><td>"+ datacliente[i].NombreCompania+"</td><td>"+ datacliente[i].NombreContacto+"</td></tr>\n";
 
                     }
-                    data2 = data.replace("{{data23}}",filas);
+                    dataretornar = data.replace("{{data}}",filas);
                     res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(data2);
+                    res.write(dataretornar);
                     return res.end();
 
                 });
